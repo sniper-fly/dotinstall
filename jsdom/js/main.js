a26012bd31574262ea9cca47d3b607207ad37428
rnakai <rnakai@student.42tokyo.jp>
2021-11-14 22:58:25 +0900

    複数要素取得

---
+++ jsdom/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+    function update() {
+        document.querySelector('h1').textContent = 'Changed!';
+        document.querySelectorAll('p').forEach((p, index) => {
+            p.textContent = `${index}番目のpです`
+        });
+    }
+
+    setTimeout(update, 1000);
+}
