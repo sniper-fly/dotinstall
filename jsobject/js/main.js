f364b3682564deaeee812da8f52c382bbf7ccab7
rnakai <rnakai@student.42tokyo.jp>
2021-11-14 18:08:40 +0900

    setInterval etc.

--- jsobject/js/main.js
+++ jsobject/js/main.js
@@ -54,3 +54,16 @@
     //     console.log('yes!!');
     // }
 }
+
+{
+    let i = 0;
+    function showTime() {
+        console.log(new Date());
+        i++;
+        if (i >= 3) {
+            clearInterval(intervalId);
+        }
+    }
+
+    const intervalId = setInterval(showTime, 1000);
+}
