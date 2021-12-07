853e593b7f472671b23d3e2977622f73150a9c99
rnakai <rnakai@student.42tokyo.jp>
2021-12-07 10:25:38 +0900

    classList, add , remove

--- jsdom/js/main.js
+++ jsdom/js/main.js
@@ -2,14 +2,20 @@
 
 {
     function update() {
-        document.querySelector('h1').textContent = 'Changed!';
+        document.querySelector('h1').textContent = 'Changaaed!';
         document.querySelectorAll('p').forEach((p, index) => {
             p.textContent = `${index}番目のpです`
         });
         const target = document.getElementById('target');
 
         target.title = 'this is title';
-        target.className = 'my-color';
+        // target.className = 'my-color';
+        // target.classList.add('my-border');
+        if (target.classList.contains('my-color') === true) {
+            target.classList.remove('my-color');
+        } else {
+            target.classList.add('my-color');
+        }
 
     }
 
