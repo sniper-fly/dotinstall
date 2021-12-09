e615603c051e2891ac49427a6e6b907c11c0efba
rnakai <rnakai@student.42tokyo.jp>
2021-12-09 16:41:49 +0900

    jquery独自メソッド

---
+++ jquery/js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$(() => {
+    $('button').click(() => {
+        // $('p').text('おはしん');
+        // $('p').addClass('red-text');
+
+        $('p').text('おはしん').addClass('red-text');
+    })
+})
