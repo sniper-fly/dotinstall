43d113eaa277c1c09841e57f9412acd069ecce8a
rnakai <rnakai@student.42tokyo.jp>
2021-12-10 16:19:29 +0900

    要素の削除

--- jquery/js/main.js
+++ jquery/js/main.js
@@ -15,5 +15,13 @@ $(() => {
 
         $('<li>').text($input.val()).appendTo('ul');
         $input.val('').focus();
+
+    })
+
+    $('ul').click(e => {
+        if (e.target.nodeName !== 'LI' || !confirm("Are you sure?")) {
+            return ;
+        }
+        $(e.target).fadeOut();
     })
 })
